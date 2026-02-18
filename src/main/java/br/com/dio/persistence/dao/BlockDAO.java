00ef9042db7899af77bc4a87b829d3f40c564b2f
Gustavo Oliveira <gustavo.h.oliveira7531@gmail.com>
2026-02-18 16:58:06 -0300

    add findByCardId

--- src/main/java/br/com/dio/persistence/dao/BlockDAO.java
+++ src/main/java/br/com/dio/persistence/dao/BlockDAO.java
@@ -1,11 +1,15 @@
 package br.com.dio.persistence.dao;
 
+import br.com.dio.dto.BlockDetailsDTO;
 import lombok.AllArgsConstructor;
 
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.time.OffsetDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
+import static br.com.dio.persistence.converter.OffsetDateTimeConverter.toOffsetDateTime;
 import static br.com.dio.persistence.converter.OffsetDateTimeConverter.toTimestamp;
 
 @AllArgsConstructor
@@ -33,6 +37,31 @@ public class BlockDAO {
             statement.setLong(i, cardId);
             statement.executeUpdate();
         }
+    }
+
+    public List<BlockDetailsDTO> findByCardId(final Long cardId) throws SQLException {
+        var sql = """
+            SELECT id, blocked_at, block_reason, unblocked_at, unblock_reason
+              FROM BLOCKS
+             WHERE card_id = ?
+             ORDER BY blocked_at ASC;
+            """;
+        var blocks = new ArrayList<BlockDetailsDTO>();
+        try (var statement = connection.prepareStatement(sql)) {
+            statement.setLong(1, cardId);
+            statement.executeQuery();
+            var rs = statement.getResultSet();
+            while (rs.next()) {
+                blocks.add(new BlockDetailsDTO(
+                        rs.getLong("id"),
+                        toOffsetDateTime(rs.getTimestamp("blocked_at")),
+                        rs.getString("block_reason"),
+                        toOffsetDateTime(rs.getTimestamp("unblocked_at")),
+                        rs.getString("unblock_reason")
+                ));
+            }
+        }
+        return blocks;
     }
 
 }
